a22cd59fdc1e18d88bf51a94cf94b81e6dfa401f
michaeljpitz <michaeljpitz@gmail.com>
2017-10-16 10:18:21 +0200

    homepage with News, view is default/index.html.twig

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,14 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+        // on récupère toutes les news
+        $em = $this->getDoctrine()->getManager();
+
+        $news = $em->getRepository('AppBundle:News')->findAll();
+
+        // on appel la vue et on y affiche les news
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+           "thenews"=>$news
         ]);
     }
 }
